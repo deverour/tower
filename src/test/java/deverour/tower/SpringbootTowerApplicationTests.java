e9a0e9d489f5292ac6506477503c55628d435e2e
deverour <361844795@qq.com>
2020-01-30 14:42:17 +0800

    V2.0

---
+++ src/test/java/deverour/tower/SpringbootTowerApplicationTests.java
@@ -0,0 +1,13 @@
+package deverour.tower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/deverour/tower/app.java
@@ -0,0 +1,9 @@
+package deverour.tower;
+
+import java.util.List;
+
+public class app {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
